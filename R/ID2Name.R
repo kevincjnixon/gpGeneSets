b10d000f4d48d2ee916e49804a31014336995bdd
kevincjnixon <60985228+kevincjnixon@users.noreply.github.com>
2021-11-11 13:39:29 -0500

    Add files via upload

--- R/ID2Name.R
+++ R/ID2Name.R
@@ -1,14 +1,13 @@
 #' Function to convert term IDs to names in data frame
-#' 
+#'
 #' @param x data.frame with a column containg GO IDs
 #' @param col.name character indicating the column name containg the GO IDs to be converted
 #' @return data.frame with new column containing GO term names from GO IDs
 #' @export
- 
+
 ID2Name<-function(x, col.name="term_name"){
- data("IDname")
   if(!col.name %in% colnames(x)){
-    stop("col.name: ",col.name," not found in x.")
+    stop("col.name not found in x.")
   }
   if(col.name=="term_name"){
     message("output column will be 'term_name', changing 'term_name' to 'term_id'...")
